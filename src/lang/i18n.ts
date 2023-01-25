24293d9d741135e232bd7b6f89d17c2f58617b84
wzBeholder <123535663+wzBeholder@users.noreply.github.com>
2023-01-25 01:55:50 -0500

    Update i18n.ts

--- src/lang/i18n.ts
+++ src/lang/i18n.ts
@@ -3,6 +3,7 @@ import { initReactI18next } from "react-i18next";
 
 import langEn from "./lang.en.json";
 import langKo from "./lang.ko.json";
+import langzh_CN from "./lang.zh_CN.json";
 
 const resource = {
   en: {
@@ -11,6 +12,9 @@ const resource = {
   ko: {
     translations: langKo,
   },
+  zh_CN: {
+    translations: langZh_CN,
+  },
 };
 
 i18n.use(initReactI18next).init({
@@ -18,6 +22,20 @@ i18n.use(initReactI18next).init({
   lng: "ko",
   //초기값
   fallbackLng: "ko",
+  debug: false,
+  defaultNS: "translations",
+  ns: "translations",
+  keySeparator: false,
+  interpolation: {
+    escapeValue: false,
+  },
+});
+
+i18n.use(initReactI18next).init({
+  resources: resource,
+  lng: "zh_CN",
+  //中文简体
+  fallbackLng: "zh_CN",
   debug: false,
   defaultNS: "translations",
   ns: "translations",
